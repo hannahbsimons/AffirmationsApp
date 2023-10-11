b0edc409b3aebeefa6e5ea9f2f8de4cc91dc64b2
Hannah Simons <hannahbsimons@gmail.com>
2023-10-11 14:57:13 -0400

    Initial Commit

---
+++ Affirmations/AffirmationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsApp.swift
+//  Affirmations
+//
+//  Created by Hannah Simons on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
